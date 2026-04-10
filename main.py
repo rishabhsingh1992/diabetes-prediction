bc9eef422e7312ba5fa37613db5114960702cf5f
Rishabh Singh <rishabhsingh12251992@gmail.com>
2026-04-10 23:20:44 +0530

    Refactor diabetes prediction model to use a pipeline; update .gitignore for model artifacts

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 df = pd.read_csv("../../Datasets/diabetes.csv")
@@ -12,28 +13,35 @@ df = pd.read_csv("../../Datasets/diabetes.csv")
 X = df.drop(columns=["Outcome"])
 y = df["Outcome"]
 
-X = df.drop(columns=["Outcome"])
-y = df["Outcome"]
 
 (X_train, X_test, y_train, y_test) = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-scaler = StandardScaler()
+# scaler = StandardScaler()
 
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+# X_train_scaled = scaler.fit_transform(X_train)
+# X_test_scaled = scaler.transform(X_test)
 
-model = DecisionTreeClassifier()
+# model = DecisionTreeClassifier()
 
-model.fit(X_train_scaled, y_train)
+# model.fit(X_train_scaled, y_train)
 
-y_pred = model.predict(X_test_scaled)
+# y_pred = model.predict(X_test_scaled)
+
+model = Pipeline(
+    [("scaler", StandardScaler()), ("classifier", DecisionTreeClassifier())]
+)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
 
 accuracy = accuracy_score(y_test, y_pred)
 
 # print(f"Accuracy: {accuracy * 100:.2f}%")
 # print(confusion_matrix(y_test, y_pred))
 
-joblib.dump(model, "models/diabetes_model.pkl")
-joblib.dump(scaler, "models/diabetes_scaler.pkl")
+joblib.dump(model, "models/diabetes_model_pipeline.pkl")
+# joblib.dump(scaler, "models/diabetes_scaler.pkl")
